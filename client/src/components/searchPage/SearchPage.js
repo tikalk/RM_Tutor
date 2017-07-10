aa5b06319f3db5c06538e83bf3e6c76da16d7a17
Alex <alexr@cyberint.com>
2017-07-10 12:39:40 +0300

    search page working

--- client/src/components/searchPage/SearchPage.js
+++ client/src/components/searchPage/SearchPage.js
@@ -3,23 +3,115 @@
  */
 
 import React from 'react';
+import Result from './../Result';
+
+const mock = {
+    defaultResults: {
+        popular: [
+            {
+                title: "Title 2",
+                author: "Oren",
+                rating: 5,
+                desc: "Description"
+            },
+            {
+                title: "Title 1",
+                author: "Oren",
+                rating: 5,
+                desc: "Description"
+            },
+            {
+                title: "Title 1",
+                author: "Oren",
+                rating: 5,
+                desc: "Description"
+            },
+            {
+                title: "Title 1",
+                author: "Oren",
+                rating: 5,
+                desc: "Description"
+            }
+        ],
+        latest: [
+            {
+                title: "Title 2",
+                author: "Oren",
+                rating: 5,
+                desc: "Description"
+            },
+            {
+                title: "Title 1",
+                author: "Oren",
+                rating: 5,
+                desc: "Description"
+            },
+            {
+                title: "Title 1",
+                author: "Oren",
+                rating: 5,
+                desc: "Description"
+            },
+            {
+                title: "Title 1",
+                author: "Oren",
+                rating: 5,
+                desc: "Description"
+            }
+        ]
+    }
+};
 
 class SearchPage extends React.Component {
     constructor(props) {
         super(props);
 
         this.state = {
-            view: 'default' // can be search results as well
+            view: 'default', // can be search results as well
+            defaultResults: mock.defaultResults
         }
     }
 
     render() {
+        console.log('this.state', this.state);
+
         return (
             <div>
                 <div>Search Component Goes Here</div>
                 <div>
                     {
-                        this.state.view === 'default' ? 'Default Screen' : 'Search Results'
+                        this.state.view === 'default' ?
+                            <div>
+                                {
+                                    this.state.defaultResults.popular.map((obj, k) => {
+                                        const {title, author, rating, desc} = obj;
+                                        return (
+                                            <Result title={title}
+                                                    author={author}
+                                                    rating={rating}
+                                                    desc={desc}
+                                                    key={k}
+                                            />
+                                        )
+                                    })
+                                }
+                            </div>
+                            :
+                            <div>
+                                {
+                                    this.state.defaultResults.latest.map((obj, k) => {
+                                        const {title, author, rating, desc} = obj;
+                                        return (
+                                            <Result title={title}
+                                                    author={author}
+                                                    rating={rating}
+                                                    desc={desc}
+                                                    key={k}
+                                            />
+                                        )
+                                    })
+                                }
+                            </div>
                     }
                 </div>
             </div>
