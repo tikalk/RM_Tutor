16006d2fb27de82490e198dd50132bba6c9afc0a
Rafael Bodill <justrafi@gmail.com>
2017-07-10 12:47:18 +0300

    Add slim-js and evyros/webco fork

--- client/src/index.js
+++ client/src/index.js
@@ -4,5 +4,8 @@ import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
+import 'slim-js'
+require('webco/components/t-stars')
+
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
