fe6cf28f743f657ecb5cf70206656d8fc66dc426
Yaniv Peleg <yaniv.p@ourcart.com>
2017-07-10 13:18:10 +0300

    no message

---
+++ Tutor_server/src/main/java/com/tikalk/rest/InviteTutorRequest.java
@@ -0,0 +1,28 @@
+package com.tikalk.rest;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by yaniv on 10/07/2017.
+ */
+public class InviteTutorRequest {
+    private String roadmapId;
+    private List<String> tutorIds = new ArrayList<>();
+
+    public String getRoadmapId() {
+        return roadmapId;
+    }
+
+    public void setRoadmapId(String roadmapId) {
+        this.roadmapId = roadmapId;
+    }
+
+    public List<String> getTutorIds() {
+        return tutorIds;
+    }
+
+    public void setTutorIds(List<String> tutorIds) {
+        this.tutorIds = tutorIds;
+    }
+}
